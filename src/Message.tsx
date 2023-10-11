78626c643fc8d175629c13724b59150e0b445b9e
thomaswinchestar <thomasanthony999@gmail.com>
2023-10-12 02:19:08 +0630

    react with tailwind, typescript, vite, bun

---
+++ src/Message.tsx
@@ -0,0 +1,10 @@
+//PascalCasing
+function Message() {
+    //JSX JavaScript XML
+    const name = '' ;
+    if (name)
+    return <h1 className="text-3xl font-bold underline">Hello {name}</h1>;
+    return <h1 className="text-3xl font-bold underline">Hello World!</h1>;
+}
+
+export default Message;
